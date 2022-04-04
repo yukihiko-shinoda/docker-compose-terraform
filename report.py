48a0eefe1adf809d675f8adbe077fe003298d0f2
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2022-04-05 00:11:17 +0900

    Support Terraform version 0.15.4 or more

--- report.py
+++ report.py
@@ -4,6 +4,7 @@ from jinja2 import Template
 
 
 REGEX_NO_DIFF = r"No\schanges\.\sInfrastructure\sis\sup-to-date\."
+REGEX_NO_DIFF_0_15_4 = r"No\schanges\.\sYour\sinfrastructure\smatches\sthe\sconfiguration\."
 REGEX_EXISTS_DIFF = r"Terraform\sused\sthe\sselected\sproviders\sto\sgenerate\sthe\sfollowing\sexecution"
 REGEX_EXISTS_DIFF_OUTPUT = r"Changes\sto\sOutputs\:"
 REGEX_EXISTS_DIFF_0_13 = r"An\sexecution\splan\shas\sbeen\sgenerated\sand\sis\sshown\sbelow\."
@@ -14,7 +15,7 @@ class Splitter:
         # ---               : for Terraform 0.14 or less
         # ───               : for Terraform 0.15 ~ 1.0.5
         # "Terraform used ~": for Terraform 1.0.6 or more
-        self.regex = re.compile(fr"({REGEX_NO_DIFF}|{REGEX_EXISTS_DIFF}|{REGEX_EXISTS_DIFF_OUTPUT}|{REGEX_EXISTS_DIFF_0_13}|{REGEX_FAILED})\n")
+        self.regex = re.compile(fr"({REGEX_NO_DIFF}|{REGEX_NO_DIFF_0_15_4}|{REGEX_EXISTS_DIFF}|{REGEX_EXISTS_DIFF_OUTPUT}|{REGEX_EXISTS_DIFF_0_13}|{REGEX_FAILED})\n")
 
     def cut(self, path_to_file: Path):
         log = path_to_file.read_text()
